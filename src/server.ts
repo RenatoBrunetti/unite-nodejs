d3c7bec007c8eea490d17c6023ffdb0b3a0f26f9
Renato Brunetti <recarapia@gmail.com>
2024-09-26 18:20:44 -0300

    feat: add fastify swagger doc

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import "dotenv/config";
+
 import fastfy from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
+  jsonSchemaTransform,
 } from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 import {
   createEvent,
@@ -16,6 +20,20 @@ import { checkIn, getAttendeeBadge } from "@/routes/attendee";
 const PORT = parseInt(process.env.PORT as string);
 
 const app = fastfy();
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    info: {
+      title: "pass.in",
+      description: "Back-end API description",
+      version: "1.0.0.",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+app.register(fastifySwaggerUi, { routePrefix: "/docs" });
+
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
